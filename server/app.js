15a67991f986a036c4ac06cf2bb8366320e9b818
ypx0352 <yuepengxiang@gmail.com>
2021-12-07 02:28:01 +1100

    cheerio

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const axios = require("axios");
 const cors = require("cors");
 const FormData = require("form-data");
 require("dotenv").config();
+const cheerio = require("cheerio");
+const fs = require("fs");
 
 const app = express();
 app.use(express.json());
@@ -34,8 +36,7 @@ const login = async () => {
 //login();
 
 // search for a specific package
-app.get("/:pk_id", async (req, res) => {
-  const { pk_id } = req.params;
+const getOnePackage = async (pk_id) => {
   var packageSearchData = new FormData();
   packageSearchData.append("msearch_text", "idnum");
   packageSearchData.append("sm_search", "1");
@@ -48,10 +49,35 @@ app.get("/:pk_id", async (req, res) => {
       data: packageSearchData,
       headers: packageSearchData.getHeaders({ cookie: cookies.toString() }),
     });
-    console.log(response);
-    res.send(cookies.toString());
+    // check response contain package information
+    const $ = cheerio.load(response.data);
+    console.log("1");
+    if ($(".wid20").children().text().trim() === pk_id.trim()) {
+      return parseHtml(response.data);
+    } else {
+      return "false";
+    }
   } catch (error) {
     console.log(error);
+    return "false";
+  }
+};
+
+app.get("/:pk_id", async (req, res) => {
+  const { pk_id } = req.params;
+  var result = await getOnePackage(pk_id);
+  for (let index = 0; index <= 3; index++) {
+    if (result != "false") {
+      res.status(200).json(result);
+      break;
+    } else {
+      if (index !== 3) {
+        await login();
+        result = await getOnePackage(pk_id);
+      } else {
+        res.send("server error");
+      }
+    }
   }
 });
 
@@ -70,3 +96,77 @@ app.get("/api/all", async (req, res) => {
 app.listen(1100, () => {
   console.log("server listening on port 1100.");
 });
+
+//parse html
+
+const data = fs.readFileSync("../client/src/test.html", "utf8");
+
+const parseHtml = (data) => {
+  const package_msg = {};
+  const $ = cheerio.load(data);
+  // package_id
+  package_msg.package_id = $(".wid20").children().text();
+
+  // weight
+  package_msg.package_weight = $("thead")
+    .children()
+    .children()
+    .eq(4)
+    .text()
+    .replace("KGs", "");
+
+  // receiver name and phone
+  const nameAndPhone = $("tbody")
+    .children()
+    .children()
+    .eq(1)
+    .children()
+    .eq(1)
+    .text()
+    .trim()
+    .replace("：", "")
+    .split("/");
+  package_msg.receiver_name = nameAndPhone[0].trim();
+  package_msg.receiver_phone = nameAndPhone[1];
+
+  // reveiver address
+  package_msg.receiver_address = $("tbody")
+    .children()
+    .children()
+    .eq(1)
+    .children()
+    .eq(2)
+    .text();
+
+  // amount
+  package_msg.item_count = $("tbody")
+    .children()
+    .children()
+    .eq(3)
+    .children()
+    .eq(0)
+    .html()
+    .split("：")[1];
+
+  // type
+  package_msg.item_type = $("tbody")
+    .children()
+    .children()
+    .eq(3)
+    .children()
+    .eq(1)
+    .html()
+    .split("：")[1];
+
+  // items
+  var items = [];
+  $(".cp_title")
+    .siblings()
+    .each((index, element) => {
+      items[index] = $(element).text();
+    });
+  package_msg.items = items;
+  return package_msg;
+};
+
+//parseHtml(data);
